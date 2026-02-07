1933ec43c9415ebb56deef4e775460a1be792204
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-08 02:03:45 +0700

    Add JwtUtils.class

--- src/main/java/com/project/spring_security/contact_management/ContactController.java
+++ src/main/java/com/project/spring_security/contact_management/ContactController.java
@@ -1,21 +1,25 @@
 package com.project.spring_security.contact_management;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping
     public String getContacts() {
         return "Returning all contacts";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public String addContact() {
         return "New contact added!";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public String deleteContact(@PathVariable int id) {
         return "Contact with id: " + id + "is deleted!";
--- src/main/java/com/project/spring_security/contact_management/security/jwt/JwtUtils.java
+++ src/main/java/com/project/spring_security/contact_management/security/jwt/JwtUtils.java
@@ -1,4 +1,41 @@
 package com.project.spring_security.contact_management.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
 public class JwtUtils {
+    
+    private static final Logger logger = LoggerFactory.getLogger("JwtUtils.class");
+
+    @Value("${spring.app.jwtSecretKey}")
+    private String jwtSecretKey;
+
+    public String generateToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        logger.debug("Authorization Header: {}", bearerToken);
+        if(bearerToken != null && bearerToken.startsWith("Bearer "))
+            return bearerToken.substring(7);
+        return null;
+    }
+
+    public String generateUsernameFromJwtToken(String jwtToken) {
+        return Jwts.parser()
+                .verifyWith(key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().getSubject();
+    }
+
+    public SecretKey key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecretKey));
+    }
 }
