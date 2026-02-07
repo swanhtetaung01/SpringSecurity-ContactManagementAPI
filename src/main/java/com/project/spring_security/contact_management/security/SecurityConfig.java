6ab5e64a2cf58e00caa860873c1f5f4f0c2bae03
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-07 16:39:14 +0700

    Add commandlinerunner for initial data injection

---
+++ src/main/java/com/project/spring_security/contact_management/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.project.spring_security.contact_management.security;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(authorizeRequests -> (authorizeRequests
+                .requestMatchers("/contacts/public/info").permitAll()
+                .anyRequest()).authenticated());
+
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public CommandLineRunner initialData(UserDetailsService userDetailsService, DataSource dataSource) {
+        return args -> {
+            JdbcUserDetailsManager manager = (JdbcUserDetailsManager) userDetailsService;
+
+            UserDetails adminUser = User.withUsername("adminUser1")
+                    .password(passwordEncoder().encode("admin1password"))
+                    .roles("ADMIN")
+                    .build();
+
+            UserDetails regularUser = User.withUsername("regularUser1")
+                    .password(passwordEncoder().encode("regular1password"))
+                    .roles("USER")
+                    .build();
+
+            JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+            userDetailsManager.createUser(adminUser);
+            userDetailsManager.createUser(regularUser);
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
