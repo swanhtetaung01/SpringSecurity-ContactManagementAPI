6fef507cfd579b53d76c0f32db5c463129f01ad3
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-08 13:26:39 +0700

    Add generateJwtTokenFromUsername method in JwtUtils

--- src/main/java/com/project/spring_security/contact_management/security/jwt/JwtUtils.java
+++ src/main/java/com/project/spring_security/contact_management/security/jwt/JwtUtils.java
@@ -8,13 +8,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class JwtUtils {
-    
+
+    @Value("${spring.app.jwtExpiration}")
+    private int jwtExpiraionMs;
+
     private static final Logger logger = LoggerFactory.getLogger("JwtUtils.class");
 
     @Value("${spring.app.jwtSecretKey}")
@@ -23,7 +28,7 @@ public class JwtUtils {
     public String generateToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         logger.debug("Authorization Header: {}", bearerToken);
-        if(bearerToken != null && bearerToken.startsWith("Bearer "))
+        if (bearerToken != null && bearerToken.startsWith("Bearer "))
             return bearerToken.substring(7);
         return null;
     }
@@ -35,7 +40,16 @@ public class JwtUtils {
                 .getPayload().getSubject();
     }
 
-    public SecretKey key(){
+    public String generateJwtTokenFromUsername(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + jwtExpiraionMs))
+                .signWith(key())
+                .compact();
+    }
+
+    public SecretKey key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecretKey));
     }
 }
